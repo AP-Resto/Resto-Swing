3eac5a486d6c3812e794484d278caa7974ecc411
MAURY Mathis <mathismaury31@gmail.com>
2024-03-14 14:22:44 +0100

    Restyling des labels pour les détails de commande

--- src/fr/test/panels/PanelDetailCommande.java
+++ src/fr/test/panels/PanelDetailCommande.java
@@ -10,19 +10,21 @@ import javax.swing.*;
 public class PanelDetailCommande extends JPanel {
 	private TableauLigneComandes tableauLignesCmd;
 	private Commande commandeSelectionnee;
-	 private JLabel idLabel; // Label pour afficher l'ID de la commande
-	 private JLabel DateLabel;
-	 private JLabel EtatLabel;
-	 private JLabel MontantLabel;
-	 private JLabel UserLabel;
+	private JLabel idLabel; // Label pour afficher l'ID de la commande
+	private JLabel DateLabel;
+	private JLabel EtatLabel;
+	private JLabel MontantLabel;
+	private JLabel UserLabel;
+
+	private Font arial = new Font("Arial", Font.PLAIN, 14);
 
 	public PanelDetailCommande() {
 		this.setLayout(null);
 		this.ajouterComposants();
 	}
 
-	private void ajouterComposants(){
-		if(this.commandeSelectionnee == null)
+	private void ajouterComposants() {
+		if (this.commandeSelectionnee == null)
 			return;
 
 		this.removeAll();
@@ -30,41 +32,41 @@ public class PanelDetailCommande extends JPanel {
 		this.tableauLignesCmd.setBounds(55, 215, 831, 350);
 		this.tableauLignesCmd.setBackground(Color.black);
 		this.add(this.tableauLignesCmd);
-		
+
 		// Ajout d'un JLabel pour afficher l'ID de la commande
-        idLabel = new JLabel("Identifiant de la commande sélectionnée : #" + commandeSelectionnee.getIdCommande());
-        idLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        idLabel.setForeground(Color.black);
-        idLabel.setBounds(20, 20, 400, 30);
-        this.add(idLabel);
-        
-        DateLabel = new JLabel("Date et heure de la commande : " + commandeSelectionnee.getDate());
-        DateLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        DateLabel.setForeground(Color.black);
-        DateLabel.setBounds(20, 50, 400, 30);
-        this.add(DateLabel);
-        
-        EtatLabel = new JLabel("Etat de la commande : " + commandeSelectionnee.getIdEtat());
-        EtatLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        EtatLabel.setForeground(Color.black);
-        EtatLabel.setBounds(20, 80, 400, 30);
-        this.add(EtatLabel);
-        
-        MontantLabel = new JLabel("Montant Total (TTC) : " + commandeSelectionnee.getTotalCommande() + " €");
-        MontantLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        MontantLabel.setForeground(Color.black);
-        MontantLabel.setBounds(20, 110, 400, 30);
-        this.add(MontantLabel);
-        
-        UserLabel = new JLabel("Auteur de la commande : " + commandeSelectionnee.getIdUser());
-        UserLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        UserLabel.setForeground(Color.black);
-        UserLabel.setBounds(20, 140, 400, 30);
-        this.add(UserLabel);
-        
+		idLabel = new JLabel(
+				"<html><b>Identifiant de la commande sélectionnée : </b>#" + commandeSelectionnee.getIdCommande());
+		idLabel.setForeground(Color.black);
+		idLabel.setFont(arial);
+		idLabel.setBounds(20, 20, 400, 30);
+		this.add(idLabel);
+
+		DateLabel = new JLabel("<html><b>Date et heure de la commande : </b>" + commandeSelectionnee.getDate());
+		DateLabel.setForeground(Color.black);
+		DateLabel.setFont(arial);
+		DateLabel.setBounds(20, 50, 400, 30);
+		this.add(DateLabel);
+
+		EtatLabel = new JLabel("<html><b>Etat de la commande : </b>" + commandeSelectionnee.getIdEtat());
+		EtatLabel.setForeground(Color.black);
+		EtatLabel.setBounds(20, 80, 400, 30);
+		EtatLabel.setFont(arial);
+		this.add(EtatLabel);
+
+		MontantLabel = new JLabel(
+				"<html><b>Montant Total (TTC) : </b>" + commandeSelectionnee.getTotalCommande() + " €");
+		MontantLabel.setForeground(Color.black);
+		MontantLabel.setBounds(20, 110, 400, 30);
+		MontantLabel.setFont(arial);
+		this.add(MontantLabel);
+
+		UserLabel = new JLabel("<html><b>Auteur de la commande : </b>" + commandeSelectionnee.getIdUser());
+		UserLabel.setForeground(Color.black);
+		UserLabel.setBounds(20, 140, 400, 30);
+		UserLabel.setFont(arial);
+		this.add(UserLabel);
+
 	}
-	
-	
 
 	public void setCommande(Commande cmd) {
 		this.commandeSelectionnee = cmd;
